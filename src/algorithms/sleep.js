92a6112d6fe022db77ebea89580806a919599723
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-03 01:31:59 +0530

    Delete src/algorithms directory

--- src/algorithms/sleep.js
+++
@@ -1,5 +0,0 @@
-export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-export const generateArray = (size) => {
-  return Array.from({ length: size }, () => Math.floor(Math.random() * 400 + 50));
-};
